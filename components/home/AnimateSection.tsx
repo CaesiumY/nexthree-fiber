39ab4049ffb997b64b15eb4a748438c8282322d6
caesiumy <dbs2636@gmail.com>
2023-05-29 03:49:50 +0900

    feat: Add animation trigger by searchParams

--- components/home/AnimateSection.tsx
+++ components/home/AnimateSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ThreeJs from "@/assets/images/threejs.png";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -8,47 +9,60 @@ import {
 } from "@/utils/motion";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import ThreeJs from "@/assets/images/threejs.png";
+import { useRouter, useSearchParams } from "next/navigation";
 import CustomButton from "../common/CustomButton";
 
 const AnimateSection = () => {
-  return (
-    <AnimatePresence initial={false}>
-      <motion.section className="home" {...slideAnimation("left")}>
-        <motion.header {...slideAnimation("down")}>
-          <Image
-            src={ThreeJs}
-            alt="threejs logo"
-            width={32}
-            height={33}
-            className="object-contain"
-            priority
-          />
-        </motion.header>
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
 
-        <motion.div className="home-content" {...headContainerAnimation}>
-          <motion.div {...headTextAnimation}>
-            <h1 className="head-text">
-              LET&apos;S <br className="hidden xl:block" /> DO IT
-            </h1>
-          </motion.div>
-          <motion.div className="flex flex-col gap-5" {...headContentAnimation}>
-            <p className="text-gray-600 max-w-md">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
-              <strong>Quisquam voluptatem quas quidem doloribus quae.</strong>{" "}
-              Quisquam voluptatum, voluptate, quibusdam, quia voluptas quod quos
-              voluptatem quas quidem doloribus quae.
-            </p>
-            <CustomButton
-              backgroundType="filled"
-              onClick={() => console.log("clicked")}
-              className="px-4 py-2 font-bold text-sm w-fit"
+  const isIntro = searchParams.get("main") !== "customizer";
+
+  const OnClickButton = () => push("?main=customizer");
+
+  return (
+    // only check for direct children
+    <AnimatePresence initial={false} mode="wait">
+      {isIntro ? (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <Image
+              src={ThreeJs}
+              alt="threejs logo"
+              width={32}
+              height={33}
+              className="object-contain"
+              priority
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET&apos;S <br className="hidden xl:block" /> DO IT
+              </h1>
+            </motion.div>
+            <motion.div
+              className="flex flex-col gap-5"
+              {...headContentAnimation}
             >
-              Customize It!
-            </CustomButton>
+              <p className="text-gray-600 max-w-md">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                <strong>Quisquam voluptatem quas quidem doloribus quae.</strong>{" "}
+                Quisquam voluptatum, voluptate, quibusdam, quia voluptas quod
+                quos voluptatem quas quidem doloribus quae.
+              </p>
+              <CustomButton
+                backgroundType="filled"
+                onClick={OnClickButton}
+                className="px-4 py-2 font-bold text-sm w-fit"
+              >
+                Customize It!
+              </CustomButton>
+            </motion.div>
           </motion.div>
-        </motion.div>
-      </motion.section>
+        </motion.section>
+      ) : null}
     </AnimatePresence>
   );
 };
