54d07023a3652f1fa00a3f40e9390050eed03039
caesiumy <dbs2636@gmail.com>
2023-05-30 02:45:44 +0900

    feat: Update framer-motion types

--- utils/motion.ts
+++ utils/motion.ts
@@ -1,36 +1,37 @@
+import { AnimationProps } from "framer-motion";
+
 export const transition = { type: "spring", duration: 0.8 };
 
 type SlideDirection = "left" | "right" | "up" | "down";
 
-export const slideAnimation = (direction: SlideDirection) => {
+export const slideAnimation = (direction: SlideDirection): AnimationProps => {
   return {
     initial: {
       x: direction === "left" ? -100 : direction === "right" ? 100 : 0,
       y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
       opacity: 0,
-      transition: { ...transition, delay: 0.5 },
     },
     animate: {
       x: 0,
       y: 0,
       opacity: 1,
-      transition: { ...transition, delay: 0 },
     },
     exit: {
       x: direction === "left" ? -100 : direction === "right" ? 100 : 0,
       y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      transition: { ...transition, delay: 0 },
     },
+    transition,
   };
 };
 
-export const headContainerAnimation = {
-  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
-  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
-  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+export const headContainerAnimation: AnimationProps = {
+  initial: { x: -100, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  exit: { x: -100, opacity: 0 },
+  transition,
 };
 
-export const headTextAnimation = {
+export const headTextAnimation: AnimationProps = {
   initial: { x: 100, opacity: 0 },
   animate: { x: 0, opacity: 1 },
   transition: {
@@ -42,7 +43,7 @@ export const headTextAnimation = {
   },
 };
 
-export const headContentAnimation = {
+export const headContentAnimation: AnimationProps = {
   initial: { y: 100, opacity: 0 },
   animate: { y: 0, opacity: 1 },
   transition: {
